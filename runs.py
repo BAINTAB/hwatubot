acd07ccd6860ba98153ac7cdf78e1331e222e8f6
BAINTAB <71552900+BAINTAB@users.noreply.github.com>
2020-09-20 02:20:52 +0900

    Update runs.py

--- runs.py
+++ runs.py
@@ -39,15 +39,15 @@ async def on_message(message):
         await message.channel.send("!섯다 시작 : 섯다를 시작합니다.\n!섯다 뽑기 : 패를 뽑고 게임에 들어갑니다.\n!섯다 패까 : 현재 게임에 들어와있는 사람들의 패를 깝니다.")
     
     if message.content == "!테스트":
-        await message.channel.send("{cards}")
+        await message.channel.send(f"{cards}")
     
     if message.content == "!섯다 시작":
         if start != 1:
             cards.clear()
             for i in range(1,11):
-                cards.append("{i}")
+                cards.append(f"{i}")
             for i in range(1,11):
-                cards.append("__"+"{i}"+"__")
+                cards.append("__"+f"{i}"+"__")
             await message.channel.send("모든 카드 섞기 완료! !섯다 뽑기 로 패를 뽑아주세요!")
             start = 1
         else:
