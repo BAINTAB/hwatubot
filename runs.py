f7e738048571643c3f9484bf5a8ef1bf9c737ea7
BAINTAB <71552900+BAINTAB@users.noreply.github.com>
2020-09-19 21:41:31 +0900

    Update runs.py

--- runs.py
+++ runs.py
@@ -21,7 +21,9 @@ async def on_message(message):
     
     if message.content == "!섯다":
         if hash(message.author) in join:
-            await message.channel.send("이미 하셨잖아요.")
+            await message.channel.send("이미 패를 받으셨습니다. 플레이어 수 : {}/{}".format(len(join),maxjoin))
+        elif len(join) >= maxjoin:
+            await message.channel.send("플레이어가 꽉 찼습니다. 다음 게임에 참가해주세요.")
         else:
             rand.clear()
             rand.append(random.randint(1,10))
@@ -32,6 +34,7 @@ async def on_message(message):
             await channel.send('{}({}), {}({}) 패가 나왔습니다!'.format(rand[0],rand[1],rand[2],rand[3]))
             player.append("{}님은 {}({}), {}({})".format(message.author,rand[0],rand[1],rand[2],rand[3]))
             join.append(hash(message.author))
+            await message.channel.send("패 전송 완료! 플레이어 수 : {}/{}".format(len(join),maxjoin))
             
     
     if message.content == "!패까":
