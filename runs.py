52932d4376c0ef3dbd52b764a3cdc2c7223656cc
BAINTAB <71552900+BAINTAB@users.noreply.github.com>
2020-09-19 21:10:30 +0900

    Update runs.py

--- runs.py
+++ runs.py
@@ -34,7 +34,7 @@ async def on_message(message):
             join.append(hash(message.author))
             
     
-    if message.content == "!실행":
+    if message.content == "!":
         for say in player:
             await message.channel.send("{}".format(say))
         player.clear()
