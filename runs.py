c749b4da7872c9c7dc745080e15277b9ffcc9b1a
BAINTAB <71552900+BAINTAB@users.noreply.github.com>
2020-09-19 22:39:10 +0900

    Update runs.py

--- runs.py
+++ runs.py
@@ -6,6 +6,7 @@ client = discord.Client()
 rand = []
 player = []
 join = []
+run = 0
 maxjoin = 10
 
 @client.event
@@ -25,7 +26,10 @@ async def on_message(message):
             await message.channel.send("이미 패를 받으셨습니다. 플레이어 수 : {}/{}".format(len(join),maxjoin))
         elif len(join) >= maxjoin:
             await message.channel.send("플레이어가 꽉 찼습니다. 다음 게임에 참가해주세요.")
+        elif run == 1:
+            await message.channel.send("{}님 너무 빨라요! 다시 쳐주세요".format(message.author.display_name))
         else:
+            run=1
             rand.clear()
             rand.append(random.randint(1,10))
             rand.append(random.randint(0,1))
@@ -36,6 +40,7 @@ async def on_message(message):
             player.append("{}님은 {}({}), {}({})".format(message.author.display_name,rand[0],rand[1],rand[2],rand[3]))
             join.append(hash(message.author))
             await message.channel.send("패 전송 완료! 플레이어 수 : {}/{}".format(len(join),maxjoin))
+            run=0
             
     
     if message.content == "!패까":
